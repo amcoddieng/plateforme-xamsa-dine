2b9b6893c70b4e7c352db7fe4c3e60da4a686691
Amadou DIENG <dieng0097@gmail.com>
2024-09-26 14:00:26 +0000

    creation d'une espace de lecture du saint coran reussi

--- src/Controller/CarrousselEventController.php
+++ src/Controller/CarrousselEventController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class CarrousselEventController extends AbstractController
 {
     #[Route('/carroussel/event', name: 'app_carroussel_event')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $eventRepository = $entityManager->getRepository(Event::class);
+        $event = $eventRepository->createQueryBuilder('p')
+            ->setMaxResults(1)
+            ->getQuery()
+            ->getResult();
         return $this->render('carroussel_event/index.html.twig', [
-            'controller_name' => 'CarrousselEventController',
+            'events' => $event,
         ]);
     }
 }
--- src/Controller/PageCoranController.php
+++ src/Controller/PageCoranController.php
@@ -1,8 +1,10 @@
 <?php
+// PageCoranController.php
 
 namespace App\Controller;
 
 use App\Entity\Coran;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,35 +14,42 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageCoranController extends AbstractController
 {
     #[Route('/page/coran', name: 'app_page_coran')]
-    public function index(Request $request,EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $sourateClick = $request->query->get('numSourate',null);
+        $sourateClick = $request->query->get('numSourate', null);
+        
         // Récupérer le repository de l'entité Coran
         $coranRepository = $entityManager->getRepository(Coran::class);
-
+        
         // Construire la requête pour récupérer les sourates distinctes
         $sourates = $coranRepository->createQueryBuilder('c')
-        ->select('c.souarte , c.numero')
-        ->groupBy('c.souarte')  // Grouping by sourate to avoid duplicates
-        ->orderBy('c.numero')
-        ->getQuery()
-        ->getResult();
-        // ici je fais la verification si le user a cliquer sur un sourate
-        $pages = [];
-        if ($sourateClick) {
+            ->select('c.souarte , c.numero')
+            ->groupBy('c.souarte')  // Groupee par sourate pour eviter les duplications
+            ->orderBy('c.numero')
+            ->getQuery()
+            ->getResult();
+        
+        // Si la requête est en AJAX, renvoyer les pages correspondantes à la sourate
+        if ($request->isXmlHttpRequest() && $sourateClick) {
             $pages = $coranRepository->createQueryBuilder('c')
-                ->where('c.numero = :numero')
-                ->setParameter('numero', $sourateClick)
-                ->orderBy('c.numero_page') // Assurez-vous que les pages sont bien ordonnées
-                ->getQuery()
-                ->getResult();
+            ->select('c.image, c.numero_page')
+            ->where('c.numero = :numero')
+            ->setParameter('numero', $sourateClick)
+            ->orderBy('c.numero_page')
+            ->getQuery()
+            ->getResult();
+        
+            
+            // Retourner les pages en format JSON
+            return new JsonResponse([
+                'pages' => $pages
+            ]);
         }
-
-        // Renvoyer le résultat à la vue Twig
+        
+        // Sinon, renvoyer la vue Twig
         return $this->render('page_coran/index.html.twig', [
             'sourates' => $sourates,
             'numSourate' => $sourateClick,
-            'pages' => $pages,  // Envoyer les pages à la vue si une sourate est cliquée
         ]);
     }
 }
